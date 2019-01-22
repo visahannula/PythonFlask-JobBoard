82a2be542a29b6266325c047c0f8928aa30538f6
Visa Hannula <visa@viha.fi>
2019-01-22 22:40:22 +0200

    add templates for html layout, sql query, show data

--- jobs/app.py
+++ jobs/app.py
@@ -28,6 +28,7 @@ def execute_sql(sql, values = (), commit = False, single = False):
 
     return results
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
@@ -38,4 +39,7 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/jobs')
 def jobs():
-    return render_template("index.html")
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name \
+                FROM job \
+                JOIN employer ON employer.id = job.employer_id')
+    return render_template("index.html", jobs=jobs)
