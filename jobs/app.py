125da812869e5d3f2fb313606058ab47683a1027
Visa Hannula <visa@viha.fi>
2019-01-24 23:21:18 +0200

    support showing single employer, reviews not included

--- jobs/app.py
+++ jobs/app.py
@@ -18,6 +18,7 @@ def open_connection():
 def execute_sql(sql, values = (), commit = False, single = False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
+    # TODO: Add prepared statement
     
     if commit == True:
         results = connection.commit()
@@ -47,3 +48,11 @@ def jobs():
 def job(job_id):
     job = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id WHERE job.id = ?', [job_id], single=True)
     return render_template("job.html", job=job)
+
+
+@app.route('/employer/<employer_id>')
+def employer(employer_id):
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
+    reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
